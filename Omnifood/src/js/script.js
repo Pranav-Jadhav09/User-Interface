da78724f561f5e274d121e5dd92dcc86cebd3e52
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-03-24 22:33:51 +0530

    added sticky nav

--- Omnifood/src/js/script.js
+++ Omnifood/src/js/script.js
@@ -2,3 +2,26 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+/////////////////////////////////////////////////////// Sticky Navigation
+const heroEL = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(heroEL);
