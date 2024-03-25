604c03ba6fdb0227bec1d9fb29e43c3dbcfc9c10
Pranav Rajesh Jadhav <iamoutput09@gmail.com>
2024-03-25 11:34:02 +0530

    media queries added

--- Omnifood/src/js/script.js
+++ Omnifood/src/js/script.js
@@ -25,3 +25,11 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(heroEL);
+
+/////////////////////////////////////////////////////// Mobile Navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
